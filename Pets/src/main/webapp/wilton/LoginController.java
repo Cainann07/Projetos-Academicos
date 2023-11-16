8d95cd0bc8424145a62c8b5b2b2e1e717704ebcd
Mateus Cainan <127872043+Cainann07@users.noreply.github.com>
2023-11-16 16:49:08 -0300

    Site de adoção
    Esse projeto consiste em um site de adoção de Pets, tendo como foco cães e gatos. Foi usado JS, HTML5 & CSS3 para o Front-End e Java para o Back-end.

---
+++ Pets/src/main/webapp/wilton/LoginController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+@WebServlet("/login")
+public class LoginController extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("teste" + req.getParameter("exampleInputPassword1"));
+		super.doPost(req, resp);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("adsf");
+		super.doGet(req, resp);
+	}
+
+}
---
+++ Script Pethouse.sql
@@ -0,0 +1,6 @@
+use Pethouse;
+create table cadastro_pessoa(id_pessoa int auto_increment primary key, email varchar(30), senha varchar(15), nome_pessoa varchar(45), telefone varchar(20));
+
+	
+select * from cadastro_pessoa;
+
